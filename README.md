17368668fb886d0313d39a5ed974eb9bc26e5fae
BuildTools <unconfigured@null.spigotmc.org>
2023-03-22 16:48:44 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # Blockify
-A Spigot plugin implementing a hopper that blockifies items
+
+A Spigot/Paper plugin implementing a Hopper that blockifies items.
+
+## What it does
+
+This Spigot/Paper plugin implements a new Hopper type called Blockifier, which holds back items that have a block form until enough items are collected to create the block. Once the required number of items are collected, the block is then created and pushed into the destination block (Chest, Hopper, etc.).
+
+Since blockable items are held back if there aren't enough present, it is possible to clog Blockifier until it cannot pull new items. Therefore, it is recommended to use Blockifier after a sorter.
+
+Here is Blockifier's crafting recipe:
+
+![Blockifier Recipe](recipe.png 'Blockifier Recipe')
+
+Blockifier looks like a Hopper, unfortunately Bukkit has no way to change the appearance of a block. When you interact with Blockifier the name will be "Blockifier" and not "Item Hopper".
+
+## Installation
+
+Copy [Blockify](target/Blockify-1.0.0-1.17%2B.jar) into the `/plugin` directory of your Spigot or Paper server.
+
+`Blockify 1.0.0` supports Minecraft versions `1.17` and above. Tested with `Minecraft 1.19.3`.
+
+## What items are blockified?
+
+Criteria used to select what conversions are allowed:
+
+-   The resulting block needs to be of the same type of material, so no material conversions
+-   The blockifying must be doable by a player with a crafting table
+
+This is a list of all conversions Blockifier does:
+
+| Source Item       | Amount | Resulting Block   |
+| ----------------- | :----: | ----------------- |
+| AMETHYST_SHARD    |   4    | AMETHYST_BLOCK    |
+| BONE_MEAL         |   9    | BONE_BLOCK        |
+| CLAY_BALL         |   4    | CLAY              |
+| COAL              |   9    | COAL_BLOCK        |
+| COPPER_INGOT      |   9    | COPPER_BLOCK      |
+| DIAMOND           |   9    | DIAMOND_BLOCK     |
+| DRIED_KELP        |   9    | DRIED_KELP_BLOCK  |
+| EMERALD           |   9    | EMERALD_BLOCK     |
+| GLOWSTONE_DUST    |   4    | GLOWSTONE         |
+| GOLD_INGOT        |   9    | GOLD_BLOCK        |
+| IRON_INGOT        |   9    | IRON_BLOCK        |
+| LAPIS_LAZULI      |   9    | LAPIS_BLOCK       |
+| MELON_SLICE       |   9    | MELON             |
+| NETHER_BRICK      |   4    | NETHER_BRICKS     |
+| NETHER_WART       |   9    | NETHER_WART_BLOCK |
+| NETHERITE_INGOT   |   9    | NETHERITE_BLOCK   |
+| POINTED_DRIPSTONE |   4    | DRIPSTONE_BLOCK   |
+| PRISMARINE_SHARD  |   4    | PRISMARINE        |
+| QUARTZ            |   4    | QUARTZ_BLOCK      |
+| RAW_COPPER        |   9    | RAW_COPPER_BLOCK  |
+| RAW_GOLD          |   9    | RAW_GOLD_BLOCK    |
+| RAW_IRON          |   9    | RAW_IRON_BLOCK    |
+| REDSTONE          |   9    | REDSTONE_BLOCK    |
+| SLIME_BALL        |   9    | SLIME_BLOCK       |
+| SNOWBALL          |   4    | SNOW_BLOCK        |
+| WHEAT             |   9    | HAY_BLOCK         |
+
+These conversions are only performed when the Hopper is pointing to a target block that is a BARREL, CHEST, DISPENSER, DROPPER, HOPPER, or a SHULKER_BOX, but not if any other block is the target or a player interacts with the inventory of Blockifier.
+
+Items that are not in this list are transported like it is a normal Hopper.
---
+++ src/main/java/com/timewaster/blockify/App.java
@@ -0,0 +1,174 @@
+package com.timewaster.blockify;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashMap;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Hopper;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryMoveItemEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.block.Block;
+import org.bukkit.Location;
+import org.bukkit.Particle;
+
+public class App extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        populateMaterialMap();
+        getServer().getPluginManager().registerEvents(this, this);
+        registerBlockifierRecipe();
+    }
+
+    @EventHandler
+    public void onItemMove(InventoryMoveItemEvent event) {
+        Inventory source = event.getSource();
+        Location sourceLocation = source.getLocation();
+        Block sourceBlock = sourceLocation.getBlock();
+        InventoryType destinationType = event.getDestination().getType();
+
+        // check if source and destination block type are correct
+        if (source.getType() == InventoryType.HOPPER
+                && sourceBlock.getType() == Material.HOPPER
+                && chestTypes.contains(destinationType)) {
+            String sourceBlockName = ((Hopper) sourceBlock.getState()).getCustomName();
+
+            // check if destination block is a Blockifier
+            if (sourceBlockName != null && sourceBlockName.equals("Blockifier")) {
+                Material stackBeingMovedMaterial = event.getItem().getType();
+
+                // check if stack being moved is blockifiable
+                if (materialTypes.contains(stackBeingMovedMaterial)) {
+                    Integer blockAmount = materialTypesWithFourItems.contains(stackBeingMovedMaterial) ? 3 : 8;
+
+                    // check if source inventory contains enough items of stack being moved
+                    if (source.contains(stackBeingMovedMaterial, blockAmount)) {
+                        source.removeItem(new ItemStack(stackBeingMovedMaterial, blockAmount));
+                        event.setItem(new ItemStack(materialConversions.get(stackBeingMovedMaterial), 1));
+                        sourceLocation.getWorld().spawnParticle(Particle.ELECTRIC_SPARK,
+                                sourceLocation.add(0.5, 0.5, 0.5), 10);
+                    } else { // if not cancel event to hold back items until enough items are available
+                        event.setCancelled(true);
+                    }
+                }
+            }
+        }
+    }
+
+    private void registerBlockifierRecipe() {
+        // create a custom hopper version
+        ItemStack blockifier = new ItemStack(Material.HOPPER, 1);
+        ItemMeta itemmeta = blockifier.getItemMeta();
+        itemmeta.setDisplayName("Blockifier");
+        ArrayList<String> lore = new ArrayList<String>();
+        itemmeta.setLore(lore);
+        itemmeta.setCustomModelData(739);
+        blockifier.setItemMeta(itemmeta);
+
+        // create crafting recipe
+        NamespacedKey recipeName = new NamespacedKey(this, "Blockifier");
+        ShapedRecipe recipe = new ShapedRecipe(recipeName, blockifier);
+        recipe.shape("IGR", "EHE", "RGI");
+        recipe.setIngredient('I', Material.IRON_BLOCK);
+        recipe.setIngredient('G', Material.GOLD_BLOCK);
+        recipe.setIngredient('R', Material.REDSTONE_BLOCK);
+        recipe.setIngredient('E', Material.EMERALD_BLOCK);
+        recipe.setIngredient('H', Material.HOPPER);
+        Bukkit.addRecipe(recipe);
+    }
+
+    // What items can be blockified
+    // Criteria:
+    // - The resulting block needs to be of the same type of material, so no
+    // material conversions
+    // - The blockifying must be doable by a player with a crafting table
+    private static final Set<Material> materialTypes = Set.of(
+            Material.AMETHYST_SHARD, // 4 > AMETHYST_BLOCK
+            Material.BONE_MEAL, // 9 > BONE_BLOCK
+            Material.CLAY_BALL, // 4 > CLAY
+            Material.COAL, // 9 > COAL_BLOCK
+            Material.COPPER_INGOT, // 9 > COPPER_BLOCK
+            Material.DIAMOND, // 9 > DIAMOND_BLOCK
+            Material.DRIED_KELP, // 9 > DRIED_KELP_BLOCK
+            Material.EMERALD, // 9 > EMERALD_BLOCK
+            Material.GLOWSTONE_DUST, // 4 > GLOWSTONE
+            Material.GOLD_INGOT, // 9 > GOLD_BLOCK
+            Material.IRON_INGOT, // 9 > IRON_BLOCK
+            Material.LAPIS_LAZULI, // 9 > LAPIS_BLOCK
+            Material.MELON_SLICE, // 9 > MELON
+            Material.NETHER_BRICK, // 4 > NETHER_BRICKS
+            Material.NETHER_WART, // 9 > NETHER_WART_BLOCK
+            Material.NETHERITE_INGOT, // 9 > NETHERITE_BLOCK
+            Material.POINTED_DRIPSTONE, // 4 > DRIPSTONE_BLOCK
+            Material.PRISMARINE_SHARD, // 4 > PRISMARINE
+            Material.QUARTZ, // 4 > QUARTZ_BLOCK
+            Material.RAW_COPPER, // 9 > RAW_COPPER_BLOCK
+            Material.RAW_GOLD, // 9 > RAW_GOLD_BLOCK
+            Material.RAW_IRON, // 9 > RAW_IRON_BLOCK
+            Material.REDSTONE, // 9 > REDSTONE_BLOCK
+            Material.SLIME_BALL, // 9 > SLIME_BLOCK
+            Material.SNOWBALL, // 4 > SNOW_BLOCK
+            Material.WHEAT // 9 > HAY_BLOCK
+    );
+
+    // Materials that use 4 instead of 9 items to create a block
+    private static final Set<Material> materialTypesWithFourItems = Set.of(
+            Material.AMETHYST_SHARD,
+            Material.CLAY_BALL,
+            Material.GLOWSTONE_DUST,
+            Material.NETHER_BRICK,
+            Material.POINTED_DRIPSTONE,
+            Material.PRISMARINE_SHARD,
+            Material.QUARTZ,
+            Material.SNOWBALL);
+
+    // Allowed destination blocks
+    private static final Set<InventoryType> chestTypes = Set.of(
+            InventoryType.BARREL,
+            InventoryType.CHEST,
+            InventoryType.DISPENSER,
+            InventoryType.DROPPER,
+            InventoryType.HOPPER,
+            InventoryType.SHULKER_BOX);
+
+    // Map of materials to convert to
+    private HashMap<Material, Material> materialConversions = new HashMap<Material, Material>();
+
+    private void populateMaterialMap() {
+        materialConversions.put(Material.AMETHYST_SHARD, Material.AMETHYST_BLOCK);
+        materialConversions.put(Material.BONE_MEAL, Material.BONE_BLOCK);
+        materialConversions.put(Material.CLAY_BALL, Material.CLAY);
+        materialConversions.put(Material.COAL, Material.COAL_BLOCK);
+        materialConversions.put(Material.COPPER_INGOT, Material.COPPER_BLOCK);
+        materialConversions.put(Material.DIAMOND, Material.DIAMOND_BLOCK);
+        materialConversions.put(Material.DRIED_KELP, Material.DRIED_KELP_BLOCK);
+        materialConversions.put(Material.EMERALD, Material.EMERALD_BLOCK);
+        materialConversions.put(Material.GLOWSTONE_DUST, Material.GLOWSTONE);
+        materialConversions.put(Material.GOLD_INGOT, Material.GOLD_BLOCK);
+        materialConversions.put(Material.IRON_INGOT, Material.IRON_BLOCK);
+        materialConversions.put(Material.LAPIS_LAZULI, Material.LAPIS_BLOCK);
+        materialConversions.put(Material.MELON_SLICE, Material.MELON);
+        materialConversions.put(Material.NETHER_BRICK, Material.NETHER_BRICKS);
+        materialConversions.put(Material.NETHER_WART, Material.NETHER_WART_BLOCK);
+        materialConversions.put(Material.NETHERITE_INGOT, Material.NETHERITE_BLOCK);
+        materialConversions.put(Material.POINTED_DRIPSTONE, Material.DRIPSTONE_BLOCK);
+        materialConversions.put(Material.PRISMARINE_SHARD, Material.PRISMARINE);
+        materialConversions.put(Material.QUARTZ, Material.QUARTZ_BLOCK);
+        materialConversions.put(Material.RAW_COPPER, Material.RAW_COPPER_BLOCK);
+        materialConversions.put(Material.RAW_GOLD, Material.RAW_GOLD_BLOCK);
+        materialConversions.put(Material.RAW_IRON, Material.RAW_IRON_BLOCK);
+        materialConversions.put(Material.REDSTONE, Material.REDSTONE_BLOCK);
+        materialConversions.put(Material.SLIME_BALL, Material.SLIME_BLOCK);
+        materialConversions.put(Material.SNOWBALL, Material.SNOW_BLOCK);
+        materialConversions.put(Material.WHEAT, Material.HAY_BLOCK);
+    }
+}
